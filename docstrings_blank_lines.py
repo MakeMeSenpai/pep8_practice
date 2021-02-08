5db178cb61bff96c56a22622ff69ec086304261a
Anthony <sean.a.protho1@gmail.com>
2021-02-07 17:31:29 -0800

    docstring

--- docstrings_blank_lines.py
+++ docstrings_blank_lines.py
@@ -1,47 +1,59 @@
 # by Kami Bigdely
 # Docstrings and blank lines
-class OnBoardTemperatureSensor:
-    VOLTAGE_TO_TEMP_FACTOR = 5.6
+class Temp_sensor:
+    volts_to_temp = 5.6 # [celsius]
     def __init__(self):
         pass
-    def read_voltage(self):        
+
+    def read_volts(self):        
         return 2.7
-    def get_temperature(self):
-        return self.read_voltage() * OnBoardTemperatureSensor.VOLTAGE_TO_TEMP_FACTOR # [celcius]
-  
-class CarbonMonoxideSensor:
-    VOLTAGE_TO_CO_FACTOR = 0.048
-    def __init__(self, temperature_sensor):
-        self.on_board_temp_sensor = temperature_sensor
-        if not self.on_board_temp_sensor:
-            self.on_board_temp_sensor = OnBoardTemperatureSensor()
-    def get_carbon_monoxide_level(self):
-        sensor_voltage = self.read_sensor_voltage()
-        self.carbon_monoxide = CarbonMonoxideSensor.convert_voltage_to_carbon_monoxide_level(sensor_voltage, self.on_board_temp_sensor.get_temperature())
-        return self.carbon_monoxide
+
+    def get_temp(self):
+        return self.read_volts() * Temp_sensor.volts_to_temp
+
+
+class Co_sensor: # Carbin Manoxide == CO
+    volts_to_co = 0.048
+    def __init__(self, temp_sensor):
+        self.temp_sensor = temp_sensor
+        if not self.temp_sensor:
+            self.temp_sensor = Temp_sensor()
+
+    def get_co_lvl(self):
+        sensor_volts = self.temp_sensor.read_volts()
+        self.co = Co_sensor.volts_to_co_lvl(sensor_volts,
+          self.temp_sensor.get_temp())
+        return self.co
+
     def read_sensor_voltage(self):
         # In real life, it should read from hardware.        
         return 2.3
-    def convert_voltage_to_carbon_monoxide_level(voltage, temperature):
-        return voltage * CarbonMonoxideSensor.VOLTAGE_TO_CO_FACTOR * temperature
-    
-class DisplayUnit:
+
+    def volts_to_co_lvl(volts, temp):
+        return volts * Co_sensor.volts_to_co * temp
+
+
+class Display_unit:
     def __init__(self):
         self.string = ''
+
     def display(self,msg):
         print(msg)
-class CarbonMonoxideDevice():
+
+
+class Co_device():
     def __init__(self, co_sensor, display_unit):
-        self.carbonMonoxideSensor = co_sensor 
-        self.display_unit = display_unit       
+        self.co_sensor = co_sensor 
+        self.display_unit = display_unit 
+
     def Display(self):
-        msg = 'Carbon Monoxide Level is : ' +  str(self.carbonMonoxideSensor.get_carbon_monoxide_level())
+        msg = 'Carbon Monoxide Level is : ' + str(self.co_sensor.get_co_lvl())
         self.display_unit.display(msg)
 
 if __name__ == "__main__":
-    temp_sensor = OnBoardTemperatureSensor()
-    co_sensor = CarbonMonoxideSensor(temp_sensor)
-    display_unit = DisplayUnit()
-    co_device = CarbonMonoxideDevice(co_sensor, display_unit)
+    temp_sensor = Temp_sensor()
+    co_sensor = Co_sensor(temp_sensor)
+    display_unit = Display_unit()
+    co_device = Co_device(co_sensor, display_unit)
     co_device.Display()
     
